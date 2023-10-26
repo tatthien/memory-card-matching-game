52593ffd05ac4f3abffdbd29d99a7ef5af18df24
Thien Nguyen <tatthien.contact@gmail.com>
2023-10-26 11:18:44 +0700

    build: add canvas-confetti to rollup build external

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
+  build: {
+    rollupOptions: {
+      external: ["node:canvas-confetti"],
+    },
+  },
 });
